ebfda416be5fdf102d651e931916a1b569b22871
Alex Reshetnyk <getupandgo11@gmail.com>
2020-08-02 22:01:32 +0300

    cmd for portdomain service

--- cmd/portdomain/main.go
+++ cmd/portdomain/main.go
@@ -1,1 +1,76 @@
-package portdomain
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+
+	portdomainV1 "port-location/api/proto/portdomain/v1"
+	"port-location/internal/portdomain"
+	"port-location/internal/portdomain/server"
+	"port-location/internal/portdomain/storage"
+)
+
+var conf = portdomain.Config{
+	GRPCServer: portdomain.GRPCServer{
+		Host: "localhost",
+		Port: "9000",
+	},
+	DB: portdomain.DB{
+		Host:          "localhost",
+		Port:          "5432",
+		Name:          "postgres",
+		Username:      "postgres",
+		Password:      "example",
+		MigrationsDir: "file:///Users/getupandgo/projects/port-location/migrations",
+	},
+}
+
+func main() {
+	psqlConf := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		conf.DB.Host, conf.DB.Port, conf.DB.Username, conf.DB.Password, conf.DB.Name,
+	)
+
+	db, err := sql.Open("postgres", psqlConf)
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	defer db.Close()
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	m, err := migrate.NewWithDatabaseInstance(conf.DB.MigrationsDir, conf.DB.Name, driver)
+	if err != nil {
+		log.Fatalf("failed to apply migrations: %v", err)
+	}
+
+	if err := m.Up(); err != nil {
+		log.Fatalf("failed to apply migrations: %v", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+
+	sc := storage.NewClient(db)
+	portDomainServer := server.NewServer(sc)
+
+	grpcServerConf := fmt.Sprintf("%s:%s", conf.GRPCServer.Host, conf.GRPCServer.Port)
+	lis, err := net.Listen("tcp", grpcServerConf)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	portdomainV1.RegisterPortDomainAPIServer(grpcServer, portDomainServer)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to start grpc server: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module port-location
 go 1.14
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.12.1
 	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/mux v1.7.4
+	github.com/lib/pq v1.3.0
 	google.golang.org/genproto v0.0.0-20200731012542-8145dea6a485
 	google.golang.org/grpc v1.31.0
 )
