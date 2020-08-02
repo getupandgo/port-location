3f6cb941077379ddacad4a637711837964c03bab
Alex Reshetnyk <getupandgo11@gmail.com>
2020-08-02 22:03:18 +0300

    skeleton of port parser service

--- internal/clientapi/parser/parser.go
+++ internal/clientapi/parser/parser.go
@@ -1,1 +1,18 @@
 package parser
+
+import (
+	"io"
+
+	"port-location/internal/clientapi/model"
+)
+
+type Service struct {
+}
+
+func (s *Service) ReadPortInfo(r io.Reader) (chan<- model.Port, error) {
+	return nil, nil
+}
+
+func (s *Service) SaveUnprocessedPort(w io.Writer) error {
+	return nil
+}
