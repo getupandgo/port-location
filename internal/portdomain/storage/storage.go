98d646e3d68deee88f07b048a600c3f98109a3d4
Alex Reshetnyk <getupandgo11@gmail.com>
2020-08-02 23:21:21 +0300

    updated db structure

--- internal/portdomain/storage/storage.go
+++ internal/portdomain/storage/storage.go
@@ -3,6 +3,9 @@ package storage
 import (
 	"context"
 	"database/sql"
+	"strconv"
+
+	"github.com/lib/pq"
 
 	"port-location/internal/portdomain/model"
 )
@@ -19,14 +22,12 @@ func NewClient(db *sql.DB) Client {
 
 func (c *Client) UpsertPort(ctx context.Context, port model.Port) error {
 	_, err := c.db.ExecContext(ctx,
-		`INSERT INTO ports (locode, name, city, country, alias, regions, coordinates, province, timezone, unlocs, foreign_code) 
-				VALUES (?,?,?,?,?,?,?,?,?,?,?) 
-				ON CONFLICT (locode) DO 
-				UPDATE SET (locode, name, city, country, alias, regions, coordinates, province, timezone, unlocs, foreign_code) = 
-				    (EXCLUDED.locode, EXCLUDED.city, EXCLUDED.country, EXCLUDED.alias, EXCLUDED.regions, EXCLUDED.coordinates, 
-				     EXCLUDED.province, EXCLUDED.timezone, EXCLUDED.unlocs,EXCLUDED.foreign_code)`,
-		port.Locode, port.Name, port.City, port.Country, port.Alias, port.Regions, port.Coordinates,
-		port.Province, port.Timezone, port.Unlocs, port.ForeignCode)
+		`INSERT INTO ports (locode, name, city, country, alias, regions, lat, lon, province, timezone, unlocs, foreign_code) 
+VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) 
+ON CONFLICT (locode) DO 
+    UPDATE SET (locode, name, city, country, alias, regions, lat, lon, province, timezone, unlocs, foreign_code) = (EXCLUDED.locode, EXCLUDED.name, EXCLUDED.city, EXCLUDED.country, EXCLUDED.alias, EXCLUDED.regions, EXCLUDED.lat, EXCLUDED.lon, EXCLUDED.province, EXCLUDED.timezone, EXCLUDED.unlocs, EXCLUDED.foreign_code)`,
+		port.Locode, port.Name, port.City, port.Country, pq.Array(port.Alias), pq.Array(port.Regions), floatToString(port.Coordinates.Lat), floatToString(port.Coordinates.Lon),
+		port.Province, port.Timezone, pq.Array(port.Unlocs), port.ForeignCode)
 
 	return err
 }
@@ -38,4 +39,8 @@ func (c *Client) GetPort(ctx context.Context, locode string) (model.Port, error)
 	}
 
 	return p, nil
+}
+
+func floatToString(n float64) string {
+	return strconv.FormatFloat(n, 'f', 6, 64)
 }
