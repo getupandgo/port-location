65b81624764de8aebc35ac8c8451930285ef0f13
Alex Reshetnyk <getupandgo11@gmail.com>
2020-08-02 16:01:56 +0300

    added cmd and config

--- cmd/clientapi/main.go
+++ cmd/clientapi/main.go
@@ -1,1 +1,36 @@
 package clientapi
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"google.golang.org/grpc"
+
+	portdomainV1 "port-location/api/proto/portdomain/v1"
+	"port-location/internal/clientapi"
+	"port-location/internal/clientapi/portdomain"
+	"port-location/internal/clientapi/server"
+)
+
+func main() {
+	conf := clientapi.Config{
+		clientapi.HTTPServer{},
+		clientapi.GRPCServer{},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctx, conf.GRPCServer.Host+conf.GRPCServer.Port, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	portDomainClient := portdomain.NewClient(portdomainV1.NewPortDomainAPIClient(conn))
+
+	s := server.NewServer(portDomainClient)
+	log.Fatal(http.ListenAndServe(":"+conf.HTTPServer.Port, s.Router))
+}
---
+++ internal/clientapi/config.go
@@ -0,0 +1,16 @@
+package clientapi
+
+type GRPCServer struct {
+	Host string `yaml:"host" validate:"required"`
+	Port string `yaml:"port" validate:"required"`
+}
+
+type HTTPServer struct {
+	Host string `yaml:"host" validate:"required"`
+	Port string `yaml:"port" validate:"required"`
+}
+
+type Config struct {
+	HTTPServer `yaml:"http_server" validate:"required"`
+	GRPCServer `yaml:"grpc_server" validate:"required"`
+}
